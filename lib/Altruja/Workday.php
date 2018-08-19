7cd333f7a6b79fd94f9337f8f0a168dd88604505
Benjamin Osoria Peralta <bnmosria@hotmail.com>
2018-08-19 23:34:20 +0200

    Clone the injected date. reformat code

--- lib/Altruja/Workday.php
+++ lib/Altruja/Workday.php
@@ -6,49 +6,57 @@ namespace Altruja;
  * A simple utility class to find the next workday from a date
  */
 
-class Workday {
+class Workday
+{
 
-  public $date, $region, $strict;
+    public $date, $region, $strict;
 
-  public function __construct(\DateTime $date, $region = 'Germany', $strict = true) {
-    $this->date = $date;
-    $this->region = $region;
-    $this->strict = $strict;
-    $class = "\\Holiday\\$this->region";
-    if (false == class_exists($class)) {
-      throw new \Exception("Region \"$this->region\" not available, please write one");
-    }
-    $this->holiday = new $class();
-  }
-
-  public function next($num = null) {
-    
-    // Move to next workday, or present day if already is workday
-    while ($this->isWeekend() || $this->isHoliday()) {
-      $this->date->add(new \DateInterval("P1D"));
+    public function __construct(
+        \DateTime $date,
+        $region = 'Germany',
+        $strict = true
+    ) {
+        $this->date = clone $date;
+        $this->region = $region;
+        $this->strict = $strict;
+        $class = "\\Holiday\\$this->region";
+        if (false == class_exists($class)) {
+            throw new \Exception("Region \"$this->region\" not available, please write one");
+        }
+        $this->holiday = new $class();
     }
 
-    // Add a number of days if applicable, always moving to the next work day
-    // In effect this adds a number of business days
-    if ($num !== null) {
-      for ($i = 0; $i < $num; $i++) {
-        $this->date->add(new \DateInterval("P1D"));
-        $this->next();
-      }
+    public function next($num = null)
+    {
+
+        // Move to next workday, or present day if already is workday
+        while ($this->isWeekend() || $this->isHoliday()) {
+            $this->date->add(new \DateInterval("P1D"));
+        }
+
+        // Add a number of days if applicable, always moving to the next work day
+        // In effect this adds a number of business days
+        if ($num !== null) {
+            for ($i = 0; $i < $num; $i++) {
+                $this->date->add(new \DateInterval("P1D"));
+                $this->next();
+            }
+        }
+
+        return $this->date;
     }
-    return $this->date;
-  }
 
-  public function isWeekend() {
-    return $this->date->format('N') >= 6;
-  }
+    public function isWeekend()
+    {
+        return $this->date->format('N') >= 6;
+    }
 
-  public function isHoliday() {
-    $holidays = $this->holiday->between($this->date, $this->date);
-    $holiday = reset($holidays);
-    $isHoliday = $holiday && ($holiday->type == \Holiday\HOLIDAY || ($this->strict == false && $holiday->type == \Holiday\NOTABLE));
-    return $isHoliday;
-  }
-
+    public function isHoliday()
+    {
+        $holidays = $this->holiday->between($this->date, $this->date);
+        $holiday = reset($holidays);
+        $isHoliday = $holiday && ($holiday->type == \Holiday\HOLIDAY || ($this->strict == false && $holiday->type == \Holiday\NOTABLE));
+        return $isHoliday;
+    }
 }
 
